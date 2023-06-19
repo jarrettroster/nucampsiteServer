4fc166d0b23c8b7170fddf8829a4a14c3c747e2f
Jarrett Roster <jarrettroster@protonmail.com>
2023-06-18 21:46:51 -0600

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
 const passportLocalMongoose = require('passport-local-mongoose');
+
+const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-    admin: {
+    firstName: {
+        type: String,
+        default: ''
+    },
+    lastName: {
+        type: String,
+        default: ''
+    },
+        admin: {
         type: Boolean,
         default: false
     }
